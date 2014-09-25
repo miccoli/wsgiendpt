255cc7baa70bc0d8bcd10cdd93e0fb94d708ce46
Stefano Miccoli <stefano.miccoli@polimi.it>
2014-09-25 23:13:47 +0200

    GPL license added

--- test.py
+++ test.py
@@ -5,9 +5,9 @@ from waitress import serve
 from wsgiref.validate import validator
 
 logging.basicConfig()
-logging.getLogger().setLevel(logging.INFO)
+logging.getLogger().setLevel(logging.DEBUG)
 
-#serve(validator(wsgiendpt.Application()), host='127.0.0.1', port='8080')
+# serve(validator(wsgiendpt.Application()), host='127.0.0.1', port='8080')
 serve(validator(wsgiendpt.Application()), unix_socket='/tmp/wait', 
                 unix_socket_perms='666')
 
